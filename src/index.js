b20eb8d80e98d2e1fb3ffba3979adcf2d80b8bac
ArthurMends777 <art50mends@gmail.com>
2025-10-20 20:16:10 -0300

    feat: Implementa rota [POST] /tasks para criar novas tarefas

--- src/index.js
+++ src/index.js
@@ -1,14 +1,35 @@
 // Importa o framework Express
 const express = require('express');
+const taskRepository = require('./taskRepository');
+
 
 const app = express();
+const PORT = 3000;
 
-const PORT = process.env.PORT || 3000;
-
+// Middleware para permitir que o Express leia JSON no corpo das requisições
 app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('API da TechFlow Solutions rodando!');
+});
+
+app.post('/tasks', (req, res) => {
+  try {
+    const { title, description } = req.body;
+
+    if (!title) {
+      return res.status(400).json({ error: 'O campo "title" é obrigatório.' });
+    }
+
+    // Usa o repositório para criar a tarefa
+    const newTask = taskRepository.createTask(title, description);
+
+    res.status(201).json(newTask);
+
+  } catch (error) {
+    res.status(500).json({ error: 'Erro interno ao criar a tarefa.' });
+  }
+
 });
 
 app.listen(PORT, () => {
